1cd029ffb3b55efc8147161c293efd0f4228c990
aleksandra elizarova <aleksa.elizarova@gmail.com>
2024-07-18 22:15:52 +0300

    fix create update dtos

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,13 +1,13 @@
 import { IsNotEmpty, IsString, MaxLength } from "class-validator"
 
 export class CreatePostDto {
-    @IsString({ message: 'Should be a string' })
-    @MaxLength(30, { message: 'Title\'s length should be less than 140' })
-    @IsNotEmpty({ message: 'Should not be empty' })
+    @IsString({ message: 'Titlee should be a string' })
+    @MaxLength(30, { message: 'Title\'s length should be less than 30' })
+    @IsNotEmpty({ message: 'Title should not be empty' })
     title: string
 
-    @IsString({ message: 'Should be a string' })
-    @MaxLength(140, { message: 'Title\'s length should be less than 140' })
-    @IsNotEmpty({ message: 'Should not be empty' })
+    @IsString({ message: 'Content should be a string' })
+    @MaxLength(140, { message: 'Content\'s length should be less than 140' })
+    @IsNotEmpty({ message: 'Content should not be empty' })
     content: string
 }
--- src/post/dto/update-post.dto.ts
+++ src/post/dto/update-post.dto.ts
@@ -1,4 +1,16 @@
+import { Optional } from "@nestjs/common"
+import { IsOptional, IsString, MaxLength, IsNotEmpty } from "class-validator"
+
 export class UpdatePostDto {
-    title: string
-    content: string
+    @IsOptional()
+    @IsString({ message: 'Title should be a string' })
+    @MaxLength(30, { message: 'Title\'s length should be less than 30' })
+    @IsNotEmpty({ message: 'Title should not be empty' })
+    title?: string
+
+    @IsOptional()
+    @IsString({ message: 'Content should be a string' })
+    @MaxLength(140, { message: 'Content\'s length should be less than 140' })
+    @IsNotEmpty({ message: 'Content should not be empty' })
+    content?: string
 }
