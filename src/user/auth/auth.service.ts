285febed97e7dcc88bc22ae6dbec5b0e33f5c691
aleksandra elizarova <aleksa.elizarova@gmail.com>
2024-07-18 13:54:31 +0300

    impl auth service

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
+interface Params {
+    email: string
+    password: string
+}
+
 @Injectable()
 export class AuthService {
     constructor(private readonly prismaService: PrismaService) {}
-    signup() {
-        
+    async signup({ email }: Params) {
+        const userExists = await this.prismaService.user.findUnique({
+            where: {
+                email
+            }
+        })
     }
 }
