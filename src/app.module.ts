28d7a1e2b7774bd93faf5c75c19c8200e2cc4628
aleksandra elizarova <aleksa.elizarova@gmail.com>
2024-07-18 17:01:46 +0300

    add post module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [UserModule, PrismaModule],
+  imports: [UserModule, PrismaModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
