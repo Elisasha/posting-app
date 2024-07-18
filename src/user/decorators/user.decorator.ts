849a7fdb95b05d2dbefbf23993cc1b135a13bccf
aleksandra elizarova <aleksa.elizarova@gmail.com>
2024-07-18 21:03:44 +0300

    add user decorator

---
+++ src/user/decorators/user.decorator.ts
@@ -0,0 +1,6 @@
+import { ExecutionContext, createParamDecorator } from "@nestjs/common"
+
+export const User = createParamDecorator((data, context: ExecutionContext) => {
+    const request = context.switchToHttp().getRequest()
+    return request.user
+})
