20de3a5c79cfc3b31d91574d1a70467962ef5091
aleksandra elizarova <aleksa.elizarova@gmail.com>
2024-07-19 02:24:02 +0300

    add bearer to interceptor

--- src/user/interceptors/user.interceptor.ts
+++ src/user/interceptors/user.interceptor.ts
@@ -4,7 +4,7 @@ import * as jwt from 'jsonwebtoken'
 export class UserInterceptor implements NestInterceptor {
     intercept(context: ExecutionContext, handler: CallHandler) {
         const request = context.switchToHttp().getRequest()
-        const token = request.headers.authorization
+        const token = request?.headers?.authorization?.split('Bearer ')[1]
         const user = jwt.decode(token)
         request.user = user
         return handler.handle()
