92e6a22b7c2f740eb148501dacbcb6d9adfd1321
aleksandra elizarova <aleksa.elizarova@gmail.com>
2024-07-19 02:55:32 +0300

    implement get all users controller

---
+++ src/user/user.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, UseGuards } from '@nestjs/common';
+import { UserService } from './user.service';
+import { Get } from '@nestjs/common'
+import { UserResponseDto } from './dtos/user-response.dto';
+import { Roles } from 'src/decorators/roles.decorator';
+import { Role } from '@prisma/client';
+import { AuthGuard } from 'src/guards/auth.guard';
+
+
+@Controller('users')
+export class UserController {
+    constructor(private readonly userService: UserService) { }
+    
+    @Roles(Role.ADMIN)
+    @UseGuards(AuthGuard)
+    @Get()
+    findAll(): Promise<UserResponseDto[]> {
+        return this.userService.getAllUsers()
+    }
+
+    
+
+
+
+
+}
