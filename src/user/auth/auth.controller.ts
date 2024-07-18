3a50e2d2687fa3663097f8996974178d419f2b5e
aleksandra elizarova <aleksa.elizarova@gmail.com>
2024-07-18 13:08:19 +0300

    add auth controller

--- src/user/auth/auth.controller.ts
+++ src/user/auth/auth.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service'
+import { SignupDto } from '../dtos/auth.dto';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
     
     @Post('/signup')
-    signup() {
+    signup(@Body() body: SignupDto) {
         return this.authService.signup()
     }
 }
