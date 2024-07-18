bb8d2ec084848dd5d2f27a51e09fc87556e7bfbe
aleksandra elizarova <aleksa.elizarova@gmail.com>
2024-07-18 15:22:30 +0300

    add create user

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
+import { Role } from '@prisma/client'
 
 interface Params {
     email: string
@@ -20,8 +21,16 @@ export class AuthService {
         if (userExists) {
             throw new ConflictException('User with the provided email already exists')
         }
+
         const saltOrRounds = 8
-        const hashedPassword = await bcrypt.hash(password, 10)
+        const hashedPassword = await bcrypt.hash(password, saltOrRounds)
+
+        const user = await this.prismaService.user.create({
+            data: {
+                email,
+                password: hashedPassword,
+            }
+        })
 
     }
 }
