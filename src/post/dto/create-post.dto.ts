02855096daa3b94c6d76ab4360c0aaf64a9159de
aleksandra elizarova <aleksa.elizarova@gmail.com>
2024-07-18 21:45:33 +0300

    add validation for create post dto

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,4 +1,13 @@
+import { IsNotEmpty, IsString, MaxLength } from "class-validator"
+
 export class CreatePostDto {
+    @IsString({ message: 'Should be a string' })
+    @MaxLength(30, { message: 'Title\'s length should be less than 140' })
+    @IsNotEmpty({ message: 'Should not be empty' })
     title: string
+
+    @IsString({ message: 'Should be a string' })
+    @MaxLength(140, { message: 'Title\'s length should be less than 140' })
+    @IsNotEmpty({ message: 'Should not be empty' })
     content: string
 }
