b02bc53c4b8f5e9ecd4c8fe19249224d55338f53
aleksandra elizarova <aleksa.elizarova@gmail.com>
2024-07-19 05:52:57 +0300

    add service method find user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -12,6 +12,16 @@ export class UserService {
         return users.map((user) => new UserResponseDto(user))
     }
 
+    async findUserById(id: number) {
+        const user = await this.prismaService.user.findUnique({
+            where: {
+                id: id
+            }
+        })
+        if (!user) {
+            throw new NotFoundException('This user doesn\'t exist')
+        }
+        return new UserResponseDto(user)
+    }
 
-    
 }
