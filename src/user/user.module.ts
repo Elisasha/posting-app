a9fa136439a94f676666cbd25e227f2e2d05e967
aleksandra elizarova <aleksa.elizarova@gmail.com>
2024-07-19 05:56:21 +0300

    add servises to module file

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [AuthController],
-  providers: [AuthService]
+  controllers: [AuthController, UserController],
+  providers: [AuthService, UserService]
 })
 export class UserModule {}
