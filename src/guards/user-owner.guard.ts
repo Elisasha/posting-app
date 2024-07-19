748ea60db3d111813796dee44c04ffc6299f2ba8
aleksandra elizarova <aleksa.elizarova@gmail.com>
2024-07-19 05:51:29 +0300

    add owner guard

---
+++ src/guards/user-owner.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable, CanActivate, ExecutionContext, ForbiddenException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Role } from '@prisma/client';
+
+@Injectable()
+export class UserOwnerGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+    canActivate(context: ExecutionContext): boolean {
+        const request = context.switchToHttp().getRequest()
+        const role = request.userRole
+        if (role !== Role.ADMIN) {
+            const userId = request.userId
+            const ownerId = parseInt(request.params.id, 10)
+    
+            if (userId !== ownerId) {
+                throw new ForbiddenException('You are not allowed to access this resource')
+            }
+        }
+
+    return true;
+  }
+}
