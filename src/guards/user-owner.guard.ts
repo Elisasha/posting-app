bbdc6da7f328001c276bee9ae982ddf2bb988921
aleksandra elizarova <aleksa.elizarova@gmail.com>
2024-07-21 11:58:02 +0300

    improove user owner guard

--- src/guards/user-owner.guard.ts
+++ src/guards/user-owner.guard.ts
@@ -1,23 +1,19 @@
 import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { Role } from '@prisma/client';
+import { RoleGuard } from './role.guard';
 
 @Injectable()
 export class UserOwnerGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
+  constructor(private reflector: Reflector, private roleGuard: RoleGuard) {}
 
-    canActivate(context: ExecutionContext): boolean {
-        const request = context.switchToHttp().getRequest()
-        const role = request.userRole
-        if (role !== Role.ADMIN) {
-            const userId = request.userId
-            const ownerId = parseInt(request.params.id, 10)
-    
-            if (userId !== ownerId) {
-                throw new ForbiddenException('You are not allowed to access this resource')
-            }
-        }
-
-    return true;
+  async canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest()
+    const userId = request.userId
+    const ownerId = parseInt(request.params.id, 10)
+    if (userId === ownerId) {
+      return true
+    } else {
+      return this.roleGuard.canActivate(context)
+    }
   }
 }
